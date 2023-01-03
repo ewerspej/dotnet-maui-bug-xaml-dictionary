be9f10f495393830393da143a6f88c29e2dba599
Julian Ewers <ewerspej@tcd.ie>
2023-01-03 12:24:02 +0100

    added solution with bug

---
+++ XamlOnlyResourceBug/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace XamlOnlyResourceBug;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        //uncomment this and it won't make a difference, either, the styles are not properly applied when x:Class is specified on Styles.xaml and Colors.xaml
+        //UpdateTheme();
+
+        MainPage = new AppShell();
+    }
+
+    private void UpdateTheme()
+    {
+        if (Current?.Resources.MergedDictionaries is not { } dictionaries)
+        {
+            return;
+        }
+
+        dictionaries.Clear();
+        dictionaries.Add(new Resources.Styles.Colors());
+        dictionaries.Add(new Resources.Styles.Styles());
+    }
+}
---
+++ XamlOnlyResourceBug/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace XamlOnlyResourceBug;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
